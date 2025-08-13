f4bb8aee997c047bfe0081b9bbf67173a3d9c15d
ledung38 <dung.lh@dsc.com.vn>
2025-08-13 08:34:22 +0700

    throttle

---
+++ src/app/performance-optimization/page.tsx
@@ -0,0 +1,7 @@
+import TrackMouse from "@/app/performance-optimization/page.client";
+
+const PerformanceOptimizationPage = () => {
+  return <TrackMouse />;
+};
+
+export default PerformanceOptimizationPage;
